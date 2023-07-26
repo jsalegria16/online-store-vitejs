d0c795833115b47a3c081ccf9acc7e4e4a67722b
JhonAlegria_Git <alegriasebas.sa@hotmail.com>
2023-07-25 20:08:35 -0500

    reactRouter

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,39 @@
 import { useState } from 'react'
+import {useRoutes, BrowserRouter} from 'react-router-dom'
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 import './App.css'
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount'
 import { MyOrder } from '../MyOrder'
 import { MyOrders } from '../MyOrders'
 import { NotFound } from '../NotFound'
+import { SingIn } from '../SingIn'
+
+const AppRoutes = () => {
+
+  let routes = useRoutes([
+    {path:"/", element:<Home />},
+    {path:'/my-order', element:<MyOrder />},
+    {path:'my-orders', element:<MyOrders />},
+    {path:'my-account', element:<MyAccount />},
+    {path:'/*', element:<NotFound />},
+    {path:'/singin', element:<SingIn />},
+  ])
+
+  return routes
+}
+
 
 function App() {
-
+  console.log('hii?');
   return (
-      <div className="bg-blue-600 text-lg text-white">
-        <Home />
-        <MyAccount/>
-        <MyOrder/>
-        <MyOrders/>
-        <NotFound/>
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
   )
 }
 
-export default App
+export  {App}
